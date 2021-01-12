f327cf446d5dfb387dafab03b45ab41e6c7e03a6
Wojtek Mach <wojtek@wojtekmach.pl>
2021-01-12 13:31:41 +0100

    Refactor

--- lib/code_info.ex
+++ lib/code_info.ex
@@ -49,35 +49,112 @@ defmodule CodeInfo do
         {{name, length(args)}, {kind, spec}}
       end
 
-    documented_types =
-      for {{:type, name, arity}, _, signature, doc, metadata} <- docs,
-          into: %{} do
-        {kind, spec} = Map.fetch!(types, {name, arity})
+    {:ok, specs} = Code.Typespec.fetch_specs(module)
+    specs = Map.new(specs)
 
-        spec_string =
-          spec
-          |> Code.Typespec.type_to_quoted()
-          |> Macro.to_string()
+    {:ok, callbacks} = Code.Typespec.fetch_callbacks(module)
+    callbacks = Map.new(callbacks)
 
-        map = %{
-          kind: kind,
-          doc: doc,
-          doc_metadata: metadata,
-          signature: signature,
-          spec_string: spec_string
-        }
+    state = %{
+      types: %{},
+      callbacks: %{},
+      macrocallbacks: %{},
+      functions: %{},
+      macros: %{},
+      doc: doc,
+      doc_metadata: metadata
+    }
 
-        map = filter(map, filter[:types])
-        {{name, arity}, map}
-      end
+    state =
+      Enum.reduce(docs, state, fn {{kind, name, arity}, _anno, signature, doc, metadata}, acc ->
+        case kind do
+          :type ->
+            {kind, spec} = Map.fetch!(types, {name, arity})
+            spec_string = type_spec_to_string(spec)
 
-    private_types =
+            map = %{
+              kind: kind,
+              doc: doc,
+              doc_metadata: metadata,
+              signature: signature,
+              spec_string: spec_string
+            }
+
+            put_in(acc, [:types, {name, arity}], filter(map, filter[:types]))
+
+          :callback ->
+            spec_strings =
+              case Map.fetch(callbacks, {name, arity}) do
+                {:ok, specs} -> Enum.map(specs, &function_spec_to_string(&1, name))
+                :error -> []
+              end
+
+            map = %{
+              doc: doc,
+              doc_metadata: metadata,
+              signature: signature,
+              spec_strings: spec_strings
+            }
+
+            put_in(acc, [:callbacks, {name, arity}], filter(map, filter[:callbacks]))
+
+          :macrocallback ->
+            spec_strings =
+              case Map.fetch(callbacks, {:"MACRO-#{name}", arity + 1}) do
+                {:ok, specs} -> Enum.map(specs, &function_spec_to_string(&1, name))
+                :error -> []
+              end
+
+            map = %{
+              doc: doc,
+              doc_metadata: metadata,
+              signature: signature,
+              spec_strings: spec_strings
+            }
+
+            put_in(acc, [:macrocallbacks, {name, arity}], filter(map, filter[:macrocallbacks]))
+
+          :function ->
+            spec_strings =
+              case Map.fetch(specs, {name, arity}) do
+                {:ok, specs} -> Enum.map(specs, &function_spec_to_string(&1, name))
+                :error -> []
+              end
+
+            map = %{
+              doc: doc,
+              doc_metadata: metadata,
+              signature: signature,
+              spec_strings: spec_strings
+            }
+
+            put_in(acc, [:functions, {name, arity}], filter(map, filter[:functions]))
+
+          :macro ->
+            spec_strings =
+              case Map.fetch(specs, {:"MACRO-#{name}", arity + 1}) do
+                {:ok, specs} -> Enum.map(specs, &function_spec_to_string(&1, name))
+                :error -> []
+              end
+
+            map = %{
+              doc: doc,
+              doc_metadata: metadata,
+              signature: signature,
+              spec_strings: spec_strings
+            }
+
+            put_in(acc, [:macros, {name, arity}], filter(map, filter[:macros]))
+
+          _ ->
+            acc
+        end
+      end)
+
+    typeps =
       for {{name, arity}, {:typep, spec}} <- types,
           into: %{} do
-        spec_string =
-          spec
-          |> Code.Typespec.type_to_quoted()
-          |> Macro.to_string()
+        spec_string = type_spec_to_string(spec)
 
         map = %{
           kind: :typep,
@@ -91,132 +168,12 @@ defmodule CodeInfo do
         {{name, arity}, map}
       end
 
-    types = Map.merge(documented_types, private_types)
+    state =
+      Enum.reduce(typeps, state, fn {{name, arity}, map}, acc ->
+        put_in(acc, [:types, {name, arity}], map)
+      end)
 
-    {:ok, specs} = Code.Typespec.fetch_specs(module)
-    specs = Map.new(specs)
-
-    functions =
-      for {{:function, name, arity}, _, signature, doc, metadata} <- docs,
-          into: %{} do
-        spec_strings =
-          case Map.fetch(specs, {name, arity}) do
-            {:ok, specs} ->
-              for spec <- specs do
-                name
-                |> Code.Typespec.spec_to_quoted(spec)
-                |> Macro.to_string()
-              end
-
-            :error ->
-              nil
-          end
-
-        map = %{
-          doc: doc,
-          doc_metadata: metadata,
-          signature: signature,
-          spec_strings: spec_strings
-        }
-
-        map = filter(map, filter[:functions])
-        {{name, arity}, map}
-      end
-
-    macros =
-      for {{:macro, name, arity}, _, signature, doc, metadata} <- docs,
-          into: %{} do
-        spec_strings =
-          case Map.fetch(specs, {:"MACRO-#{name}", arity + 1}) do
-            {:ok, specs} ->
-              for spec <- specs do
-                name
-                |> Code.Typespec.spec_to_quoted(spec)
-                |> Macro.to_string()
-              end
-
-            :error ->
-              []
-          end
-
-        map = %{
-          doc: doc,
-          doc_metadata: metadata,
-          signature: signature,
-          spec_strings: spec_strings
-        }
-
-        map = filter(map, filter[:macros])
-        {{name, arity}, map}
-      end
-
-    {:ok, callbacks} = Code.Typespec.fetch_callbacks(module)
-    callbacks = Map.new(callbacks)
-
-    regular_callbacks =
-      for {{:callback, name, arity}, _anno, signature, doc, metadata} <- docs,
-          into: %{} do
-        spec_strings =
-          case Map.fetch(callbacks, {name, arity}) do
-            {:ok, specs} ->
-              for spec <- specs do
-                name
-                |> Code.Typespec.spec_to_quoted(spec)
-                |> Macro.to_string()
-              end
-
-            :error ->
-              nil
-          end
-
-        map = %{
-          doc: doc,
-          doc_metadata: metadata,
-          signature: signature,
-          spec_strings: spec_strings
-        }
-
-        map = filter(map, filter[:callbacks])
-        {{name, arity}, map}
-      end
-
-    macro_callbacks =
-      for {{:macrocallback, name, arity}, _anno, signature, doc, metadata} <- docs,
-          into: %{} do
-        spec_strings =
-          case Map.fetch(callbacks, {:"MACRO-#{name}", arity + 1}) do
-            {:ok, specs} ->
-              for spec <- specs do
-                name
-                |> Code.Typespec.spec_to_quoted(spec)
-                |> Macro.to_string()
-              end
-
-            :error ->
-              nil
-          end
-
-        map = %{
-          doc: doc,
-          doc_metadata: metadata,
-          signature: signature,
-          spec_strings: spec_strings
-        }
-
-        map = filter(map, filter[:macrocallbacks])
-        {{name, arity}, map}
-      end
-
-    %{
-      functions: functions,
-      macros: macros,
-      callbacks: regular_callbacks,
-      macrocallbacks: macro_callbacks,
-      types: types,
-      doc: doc,
-      doc_metadata: metadata
-    }
-    |> top_filter(filter)
+    top_filter(state, filter)
   end
 
   defp top_filter(thing, filter) do
@@ -242,5 +199,13 @@ defmodule CodeInfo do
     else
       thing
     end
+  end
+
+  defp function_spec_to_string(spec, name) do
+    name |> Code.Typespec.spec_to_quoted(spec) |> Macro.to_string()
+  end
+
+  defp type_spec_to_string(spec) do
+    spec |> Code.Typespec.type_to_quoted() |> Macro.to_string()
   end
 end
