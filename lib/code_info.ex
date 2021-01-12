766555742e8e7f00935ec2bc1ebcaf24429794f7
Wojtek Mach <wojtek@wojtekmach.pl>
2021-01-12 12:54:43 +0100

    Add opaque, typep, callback, macro, macrocallback, and macro support

--- lib/code_info.ex
+++ lib/code_info.ex
@@ -44,18 +44,18 @@ defmodule CodeInfo do
     {:ok, types} = Code.Typespec.fetch_types(module)
 
     types =
-      for {_kind, {name, _, args} = spec} <- types,
+      for {kind, {name, _, args} = spec} <- types,
           into: %{} do
-        {{name, length(args)}, spec}
+        {{name, length(args)}, {kind, spec}}
       end
 
-    types =
-      for {{kind, name, arity}, _, signature, doc, metadata} <- docs,
-          kind in [:type, :opaque, :typep],
+    documented_types =
+      for {{:type, name, arity}, _, signature, doc, metadata} <- docs,
           into: %{} do
+        {kind, spec} = Map.fetch!(types, {name, arity})
+
         spec_string =
-          types
-          |> Map.fetch!({name, arity})
+          spec
           |> Code.Typespec.type_to_quoted()
           |> Macro.to_string()
 
@@ -70,6 +70,28 @@ defmodule CodeInfo do
         map = filter(map, filter[:types])
         {{name, arity}, map}
       end
+
+    private_types =
+      for {{name, arity}, {:typep, spec}} <- types,
+          into: %{} do
+        spec_string =
+          spec
+          |> Code.Typespec.type_to_quoted()
+          |> Macro.to_string()
+
+        map = %{
+          kind: :typep,
+          doc: :none,
+          doc_metadata: %{},
+          signature: [],
+          spec_string: spec_string
+        }
+
+        map = filter(map, filter[:types])
+        {{name, arity}, map}
+      end
+
+    types = Map.merge(documented_types, private_types)
 
     {:ok, specs} = Code.Typespec.fetch_specs(module)
     specs = Map.new(specs)
@@ -101,8 +123,95 @@ defmodule CodeInfo do
         {{name, arity}, map}
       end
 
+    macros =
+      for {{:macro, name, arity}, _, signature, doc, metadata} <- docs,
+          into: %{} do
+        spec_strings =
+          case Map.fetch(specs, {:"MACRO-#{name}", arity + 1}) do
+            {:ok, specs} ->
+              for spec <- specs do
+                name
+                |> Code.Typespec.spec_to_quoted(spec)
+                |> Macro.to_string()
+              end
+
+            :error ->
+              []
+          end
+
+        map = %{
+          doc: doc,
+          doc_metadata: metadata,
+          signature: signature,
+          spec_strings: spec_strings
+        }
+
+        map = filter(map, filter[:macros])
+        {{name, arity}, map}
+      end
+
+    {:ok, callbacks} = Code.Typespec.fetch_callbacks(module)
+    callbacks = Map.new(callbacks)
+
+    regular_callbacks =
+      for {{:callback, name, arity}, _anno, signature, doc, metadata} <- docs,
+          into: %{} do
+        spec_strings =
+          case Map.fetch(callbacks, {name, arity}) do
+            {:ok, specs} ->
+              for spec <- specs do
+                name
+                |> Code.Typespec.spec_to_quoted(spec)
+                |> Macro.to_string()
+              end
+
+            :error ->
+              nil
+          end
+
+        map = %{
+          doc: doc,
+          doc_metadata: metadata,
+          signature: signature,
+          spec_strings: spec_strings
+        }
+
+        map = filter(map, filter[:callbacks])
+        {{name, arity}, map}
+      end
+
+    macro_callbacks =
+      for {{:macrocallback, name, arity}, _anno, signature, doc, metadata} <- docs,
+          into: %{} do
+        spec_strings =
+          case Map.fetch(callbacks, {:"MACRO-#{name}", arity + 1}) do
+            {:ok, specs} ->
+              for spec <- specs do
+                name
+                |> Code.Typespec.spec_to_quoted(spec)
+                |> Macro.to_string()
+              end
+
+            :error ->
+              nil
+          end
+
+        map = %{
+          doc: doc,
+          doc_metadata: metadata,
+          signature: signature,
+          spec_strings: spec_strings
+        }
+
+        map = filter(map, filter[:macrocallbacks])
+        {{name, arity}, map}
+      end
+
     %{
       functions: functions,
+      macros: macros,
+      callbacks: regular_callbacks,
+      macrocallbacks: macro_callbacks,
       types: types,
       doc: doc,
       doc_metadata: metadata
--- test/code_info_test.exs
+++ test/code_info_test.exs
@@ -2,12 +2,108 @@ defmodule CodeInfoTest do
   use ExUnit.Case, async: true
 
   test "get/2" do
-    info =
-      CodeInfo.get(
-        Version,
-        [:doc, functions: [:signature], types: [:spec_string]]
-      )
+    c(~S"""
+    defmodule M do
+      @moduledoc "M docs"
 
-    IO.inspect(info, printable_limit: 50)
+      @typedoc "type1 docs"
+      @type type1() :: integer()
+
+      @typedoc "opaque1 docs"
+      @opaque opaque1() :: typep1()
+
+      @typep typep1() :: integer()
+
+      @doc "callback1 docs"
+      @callback callback1() :: :ok
+
+      @doc "macrocallback1 docs"
+      @macrocallback macrocallback1() :: :ok
+
+      @doc "function1 docs"
+      @doc since: "1.0.0"
+      @spec function1() :: :ok
+      def function1() do
+        :ok
+      end
+
+      @doc "macro1 docs"
+      @spec macro1() :: :ok
+      defmacro macro1() do
+        :ok
+      end
+    end
+    """)
+
+    info = CodeInfo.get(M)
+
+    assert info.doc == %{"en" => "M docs"}
+    assert info.doc_metadata == %{}
+
+    assert Map.fetch!(info.types, {:type1, 0}) == %{
+             kind: :type,
+             doc: %{"en" => "type1 docs"},
+             doc_metadata: %{},
+             signature: [],
+             spec_string: "type1() :: integer()"
+           }
+
+    assert Map.fetch!(info.types, {:opaque1, 0}) == %{
+             kind: :opaque,
+             doc: %{"en" => "opaque1 docs"},
+             doc_metadata: %{opaque: true},
+             signature: [],
+             spec_string: "opaque1() :: typep1()"
+           }
+
+    assert Map.fetch!(info.types, {:typep1, 0}) == %{
+             kind: :typep,
+             doc: :none,
+             doc_metadata: %{},
+             signature: [],
+             spec_string: "typep1() :: integer()"
+           }
+
+    assert Map.fetch!(info.callbacks, {:callback1, 0}) == %{
+             doc: %{"en" => "callback1 docs"},
+             doc_metadata: %{},
+             signature: [],
+             spec_strings: ["callback1() :: :ok"]
+           }
+
+    assert Map.fetch!(info.macrocallbacks, {:macrocallback1, 0}) == %{
+             doc: %{"en" => "macrocallback1 docs"},
+             doc_metadata: %{},
+             signature: [],
+             # TODO: remove macro first arg
+             spec_strings: ["macrocallback1(term()) :: :ok"]
+           }
+
+    assert Map.fetch!(info.functions, {:function1, 0}) == %{
+             doc: %{"en" => "function1 docs"},
+             doc_metadata: %{since: "1.0.0"},
+             signature: ["function1()"],
+             spec_strings: ["function1() :: :ok"]
+           }
+
+    assert Map.fetch!(info.macros, {:macro1, 0}) == %{
+             doc: %{"en" => "macro1 docs"},
+             doc_metadata: %{},
+             signature: ["macro1()"],
+             # TODO: remove macro first arg
+             spec_strings: ["macro1(term()) :: :ok"]
+           }
+  end
+
+  defp c(code) do
+    [{module, bytecode}] = Code.compile_string(code)
+    beam_path = "#{module}.beam"
+    File.write!(beam_path, bytecode)
+
+    on_exit(fn ->
+      :code.purge(module)
+      :code.delete(module)
+      File.rm!(beam_path)
+    end)
   end
 end
