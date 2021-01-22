68da07c94915b39799d34e740f9f18347e0cc0e0
Wojtek Mach <wojtek@wojtekmach.pl>
2021-01-22 15:50:46 +0100

    Extract TestHelper

--- test/code_info_test.exs
+++ test/code_info_test.exs
@@ -2,7 +2,7 @@ defmodule CodeInfoTest do
   use ExUnit.Case, async: true
 
   test "fetch/2: Elixir module" do
-    elixirc(~S"""
+    TestHelper.elixirc(~S"""
     defmodule M do
       @moduledoc "M docs"
 
@@ -121,7 +121,7 @@ defmodule CodeInfoTest do
   end
 
   test "fetch/2: Erlang module with chunk" do
-    erlc(:module1, ~S"""
+    TestHelper.erlc(:module1, ~S"""
     %% @doc module1 docs.
     -module(module1).
     -export([function1/0, function2/1]).
@@ -146,7 +146,7 @@ defmodule CodeInfoTest do
       X.
     """)
 
-    edoc_to_chunk(:module1)
+    TestHelper.edoc_to_chunk(:module1)
     {:ok, info} = CodeInfo.fetch(:module1)
 
     assert info.doc == %{
@@ -211,7 +211,7 @@ defmodule CodeInfoTest do
   end
 
   test "fetch/2: Erlang module without chunk" do
-    erlc(:module2, ~S"""
+    TestHelper.erlc(:module2, ~S"""
     -module(module2).
     -export([function1/0]).
     -export_type([type1/0]).
@@ -264,64 +264,5 @@ defmodule CodeInfoTest do
 
   test "fetch/2: Unknown module" do
     assert CodeInfo.fetch(Unknown) == {:error, :module_not_found}
-  end
-
-  defp elixirc(code) do
-    [{module, bytecode}] = Code.compile_string(code)
-    dir = tmp_dir(code)
-    beam_path = '#{dir}/#{module}.beam'
-    File.write!(beam_path, bytecode)
-    true = :code.add_path(dir)
-
-    on_exit(fn ->
-      :code.purge(module)
-      :code.delete(module)
-      File.rm_rf!(dir)
-    end)
-
-    :ok
-  end
-
-  defp erlc(module, code) do
-    dir = tmp_dir(code)
-    source_path = Path.join(dir, '#{module}.erl') |> String.to_charlist()
-    File.write!(source_path, code)
-    {:ok, module} = :compile.file(source_path, [:debug_info, outdir: dir])
-    true = :code.add_path(dir)
-
-    on_exit(fn ->
-      :code.purge(module)
-      :code.delete(module)
-      File.rm_rf!(dir)
-    end)
-
-    :ok
-  end
-
-  defp edoc_to_chunk(module) do
-    source_path = module.module_info(:compile)[:source]
-    beam_path = :code.which(module)
-    dir = :filename.dirname(source_path)
-    xml_path = '#{dir}/#{module}.xml'
-    chunk_path = '#{dir}/#{module}.chunk'
-
-    docgen_dir = :code.lib_dir(:erl_docgen)
-    cmd!("escript #{docgen_dir}/priv/bin/xml_from_edoc.escript -dir #{dir} #{source_path}")
-
-    :docgen_xml_to_chunk.main(["app", xml_path, beam_path, "", chunk_path])
-    docs_chunk = File.read!(chunk_path)
-    {:ok, ^module, chunks} = :beam_lib.all_chunks(beam_path)
-    {:ok, beam} = :beam_lib.build_module([{'Docs', docs_chunk} | chunks])
-    File.write!(beam_path, beam)
-  end
-
-  defp tmp_dir(code) do
-    dir = Path.join("tmp", :crypto.hash(:sha256, code) |> Base.url_encode64(case: :lower))
-    File.mkdir_p!(dir)
-    String.to_charlist(dir)
-  end
-
-  defp cmd!(command) do
-    0 = Mix.shell().cmd(command)
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,62 @@
 ExUnit.start()
+
+defmodule TestHelper do
+  def elixirc(code) do
+    [{module, bytecode}] = Code.compile_string(code)
+    dir = tmp_dir(code)
+    beam_path = '#{dir}/#{module}.beam'
+    File.write!(beam_path, bytecode)
+    true = :code.add_path(dir)
+
+    ExUnit.Callbacks.on_exit(fn ->
+      :code.purge(module)
+      :code.delete(module)
+      File.rm_rf!(dir)
+    end)
+
+    :ok
+  end
+
+  def erlc(module, code) do
+    dir = tmp_dir(code)
+    source_path = Path.join(dir, '#{module}.erl') |> String.to_charlist()
+    File.write!(source_path, code)
+    {:ok, module} = :compile.file(source_path, [:debug_info, outdir: dir])
+    true = :code.add_path(dir)
+
+    ExUnit.Callbacks.on_exit(fn ->
+      :code.purge(module)
+      :code.delete(module)
+      File.rm_rf!(dir)
+    end)
+
+    :ok
+  end
+
+  def edoc_to_chunk(module) do
+    source_path = module.module_info(:compile)[:source]
+    beam_path = :code.which(module)
+    dir = :filename.dirname(source_path)
+    xml_path = '#{dir}/#{module}.xml'
+    chunk_path = '#{dir}/#{module}.chunk'
+
+    docgen_dir = :code.lib_dir(:erl_docgen)
+    cmd!("escript #{docgen_dir}/priv/bin/xml_from_edoc.escript -dir #{dir} #{source_path}")
+
+    :docgen_xml_to_chunk.main(["app", xml_path, beam_path, "", chunk_path])
+    docs_chunk = File.read!(chunk_path)
+    {:ok, ^module, chunks} = :beam_lib.all_chunks(beam_path)
+    {:ok, beam} = :beam_lib.build_module([{'Docs', docs_chunk} | chunks])
+    File.write!(beam_path, beam)
+  end
+
+  defp tmp_dir(code) do
+    dir = Path.join("tmp", :crypto.hash(:sha256, code) |> Base.url_encode64(case: :lower))
+    File.mkdir_p!(dir)
+    String.to_charlist(dir)
+  end
+
+  defp cmd!(command) do
+    0 = Mix.shell().cmd(command)
+  end
+end
